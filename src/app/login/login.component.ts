1a84338fc6c47b9227a421c8f0db783ecaf5377d
sanket5 <67897088+sanket5@users.noreply.github.com>
2021-04-15 11:26:32 +0530

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+  loginForm:FormGroup
+
+  ngOnInit(): void {
+    this.createForm()
+  }
+
+  //Creating form for login
+  createForm(){
+    this.loginForm = this.fb.group({
+      email:['',],
+      password:['',]
+    })
+  }
+
+
+  onLogin(){
+    console.log(this.loginForm.value);
+    
+  }
+
+}
